d9d2a5cb87db3bffb24d0cf554e907bdd1374777
Joe Cristian Jamis <85830010+JangManWol-source@users.noreply.github.com>
2022-06-06 11:43:55 +0800

    test

--- src/UI/Card/Card.jsx
+++ src/UI/Card/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import classes from './Card.module.css'
 import { motion } from 'framer-motion'
+
 const Card = (props) => {
+
   return (
-    <motion.div className={`${classes.card} ${props.className}`} whileHover={{scale:1.2}} whileTap={{scale:1.2}} whileInView={{
+    <motion.div className={`${classes.card} ${props.className}`} whileHover={{scale:1.2}} whileTap={{scale:1.2}} initial={{opacity:0, x:100}} whileInView={{opacity:1, x:0, animationDelay:1
       
     }}>{props.children}</motion.div>
   )
